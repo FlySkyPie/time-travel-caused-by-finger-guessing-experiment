f697ecd3dd90d5d4f15c5a9e89170bb245e5aa9f
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2020-10-10 22:15:40 +0800

    Add formula

--- src/run-model-formula.js
+++ src/run-model-formula.js
@@ -1,1 +1,31 @@
- 
+import * as fs from 'fs';
+import { RandomWalkDistribution } from './RandomWalkDistribution';
+
+if (!fs.existsSync('./data')) {
+    fs.mkdirSync('./data');
+}
+
+if (!fs.existsSync('./data/formular')) {
+    fs.mkdirSync('./data/formular');
+}
+
+let max = 4700,
+    resolution = 20,
+    step = max / resolution,
+    trialTimesPerPeople = 1000000,
+    distribution = new Map(),
+    randomWalkDistribution = new RandomWalkDistribution(trialTimesPerPeople);
+
+for (let i = 0; i < resolution; i++) {
+    let x = step * i;
+    distribution.set(x, randomWalkDistribution.getPDF(x));
+}
+
+let string = "";
+distribution.forEach((value, key) => {
+    string += key.toString() + "," + value.toString() + "\n";
+});
+
+fs.writeFile("./data/formular/data.txt", string, (err) => {
+    if (err) return console.log(err);
+});
