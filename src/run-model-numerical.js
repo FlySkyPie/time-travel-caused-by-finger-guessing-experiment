27dbd4ae2bd68f672d676d1f5c5a93bd9389181f
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2020-10-10 22:54:19 +0800

    Add x axis info to data and progress print out.

--- src/run-model-numerical.js
+++ src/run-model-numerical.js
@@ -2,13 +2,13 @@ import * as fs from 'fs';
 
 /**
  * 
- * @param {array<number>} data 
+ * @param {map<number, number>|array<number>} data 
  * @param {string} fileName 
  */
 function saveData(data, fileName) {
     let string = "";
-    data.forEach(value => {
-        string += value.toString() + "\n";
+    data.forEach((value, key) => {
+        string += key.toString() + ", " + value.toString() + "\n";
     });
 
     fs.writeFile(fileName, string, (err) => {
@@ -22,17 +22,22 @@ function saveData(data, fileName) {
  * @param {number} times 
  */
 function doResursion(data, trialTimesPerPeople) {
+    process.stdout.write('processing ' + data.length.toString() + " data");
     //console.log('processing...', data.length.toString());
-   // console.log('processing...', trialTimesPerPeople);
+    // console.log('processing...', trialTimesPerPeople);
     data.fill(0);
 
     let times = data.length * trialTimesPerPeople;
     for (let i = 0; i < times; i++) {
+        if (i % Math.floor(times / 100) === 0) {
+            process.stdout.write('.');
+        }
         let randIndex = Math.floor(Math.random() * data.length),
             randTrial = Math.floor(Math.random() * 100);
 
         data[randIndex] += (randTrial >= 50) ? 1 : -1;
     }
+    process.stdout.write("\n");
 
     data.sort((a, b) => (b - a));
     let recomputeArray = data.filter(value => (value < 0));
@@ -44,26 +49,29 @@ function doResursion(data, trialTimesPerPeople) {
 let peopleAmount = 1000,
     trialTimesPerPeople = 1000000;
 
-let array = new Array(peopleAmount).fill(0),
-    statistic = new Array(20).fill(0);
+let array = new Array(peopleAmount).fill(0);
 
-let result = doResursion(array,  trialTimesPerPeople);
+let result = doResursion(array, trialTimesPerPeople);
 
-saveData(result, "./data/model-numerical/data3.txt");
+//saveData(result, "./data/model-numerical/data3.txt");
 result.sort((a, b) => (b - a));
 saveData(result, "./data/model-numerical/data1.txt");
 
 let max = Math.max(...result),
-    min = Math.min(...result),
-    delta = Math.ceil((max - min) / 20);
+    delta = Math.ceil(max / 20),
+    statistic = new Map();
 
 result.forEach(value => {
-    let index = Math.floor((value - min) / delta);
-    index = (index === 20) ? 19 : index;
-    statistic[index] += 1;
+    let index = Math.floor(value / delta) * delta;
+    if (statistic.has(index)) {
+        let value = statistic.get(index) + 1;
+        statistic.set(index, value);
+    } else {
+        statistic.set(index, 1);
+    }
 })
 
-statistic.forEach((value, index) => {
-    statistic[index] = (value / peopleAmount);
+statistic.forEach((value, key) => {
+    statistic.set(key, value / peopleAmount);
 });
-saveData(statistic, "./data/model-numerical/data2.txt");
+saveData(statistic, "./data/model-numerical/distribution1.txt");
